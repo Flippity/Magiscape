d9d117ac7682c5eb439dd57ec52c2a8b63401481
Flippity <brockhefke123@gmail.com>
2018-06-11 18:18:50 -0400

    Improved Render Code
    Added a few renderer classes and has a square being rendered using triangles

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -3,17 +3,41 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 	public static void main(String [] args) {
 		//calls method that will open the display
 		DisplayManager.createDisplay();
 		
+		Loader loader = new Loader();
+		Renderer render = new Renderer();
+		
+		float[] vertices = {
+				-0.5f, 0.5f, 0,
+				-0.5f, -0.5f, 0,
+				0.5f, -0.5f, 0,
+				0.5f, 0.5f, 0
+		};
+		
+		int[] indices = {
+			0, 1, 3, 3, 1, 2	
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, indices);
+		
 		//game loop
 		while(!Display.isCloseRequested()) {
+			render.prepare();
+			render.render(model);
 			//updates the display
 			DisplayManager.updateDisplay();
 		}
+		
+		loader.cleanUp();
+		
 		//closes the display after the loop is over
 		DisplayManager.closeDisplay();
 	}
