6f327fc36d810a6307d2931405fb6245b3b6588d
Flippity <brockhefke123@gmail.com>
2018-06-11 22:40:42 -0400

    Added shaders
    Added shaders to work with the model renderering

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -6,6 +6,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 	public static void main(String [] args) {
@@ -14,6 +15,7 @@ public class MainGameLoop {
 		
 		Loader loader = new Loader();
 		Renderer render = new Renderer();
+		StaticShader shader = new StaticShader();	
 		
 		float[] vertices = {
 				-0.5f, 0.5f, 0,
@@ -31,11 +33,13 @@ public class MainGameLoop {
 		//game loop
 		while(!Display.isCloseRequested()) {
 			render.prepare();
+			shader.start();
 			render.render(model);
+			shader.stop();
 			//updates the display
 			DisplayManager.updateDisplay();
 		}
-		
+		shader.cleanUp();
 		loader.cleanUp();
 		
 		//closes the display after the loop is over
